05f2349160ec222039fb9e37f014d04042b32cc2
bouwerja <bouwerjason1@gmail.com>
2025-06-22 21:14:30 +0200

    unsaved changes

--- database_management.py
+++ database_management.py
@@ -1,1 +1,15 @@
 import mysql.connector
+import settings as s
+
+connection = mysql.connector.connect(
+        host = s.DATABASE_HOSTNAME, 
+        user = s.ACTIVE_USERNAME,
+        password = s.ACTIVE_USER_PWD,
+        database = s.ACTIVE_DATABASE
+        )
+def connection_status():
+    if connection.is_connected():
+        mysql_cursor = connection.cursor()
+        return mysql_cursor, False
+    else:
+        return None, True

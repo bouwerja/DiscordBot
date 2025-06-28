54ff124d69f7eccf1daecd039eb943a1525bc6c7
bouwerja <bouwerjason1@gmail.com>
2025-06-28 10:33:01 +0200

    Effective balance calculation

--- database_management.py
+++ database_management.py
@@ -7,9 +7,27 @@ connection = mysql.connector.connect(
         password = s.ACTIVE_USER_PWD,
         database = s.ACTIVE_DATABASE
         )
-def connection_status():
+def dm_ConnectionStatus():
     if connection.is_connected():
         mysql_cursor = connection.cursor()
         return mysql_cursor, False
     else:
         return None, True
+
+def cal_Balance(amount):
+    cursor, err = dm_ConnectionStatus()
+    if err:
+        print("Failed to connect to database")
+        return
+    
+    select_query = """
+        SELECT SUM(fd.Balance)
+        FROM ForFun.FinanceDetail fd
+        WHERE FinID <> (SELECT MAX(FinID) FROM ForFun.FinanceDetail fd2)
+    """
+    cursor.execute(select_query)
+    current_balance = cursor.fetchall()
+    current_balance = float(current_balance[0][0])
+    
+    new_balance = current_balance - amount
+    return new_balance
